32a43993a4b767256f4d7aed951c8e624fefd593
yuta <51759549+yumarun@users.noreply.github.com>
2021-05-25 04:52:42 +0900

    初心者講座１のスクリプト追加

---
+++ 初心者講座_1/GoalController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoalController : MonoBehaviour
+{
+
+    [SerializeField] Text ClearText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            ClearText.text = "GAME CLEAR!!!";
+        }    
+    }
+}
---
+++ 初心者講座_1/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ジャンプ
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(0, 500, 0);
+        }
+
+        //左移動
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-0.015f, 0, 0);
+        }
+           
+        //右移動
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(0.015f, 0, 0);
+        }
+
+        //落下したときの処理
+        if (transform.position.y <= -10)
+        {
+            SceneManager.LoadScene("SampleScene");
+        } 
+    }
+}
